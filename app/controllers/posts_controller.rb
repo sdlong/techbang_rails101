10333710dd6537391a165f4406ceebd321e41013
sdlong <sdlong.jeng@gmail.com>
2014-12-01 17:48:25 +0800

    posts/edit and update done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
     @post = @group.posts.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
@@ -12,6 +17,17 @@ class PostsController < ApplicationController
       redirect_to group_path(@group), notice: "新增文章成功！"
     else
       render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
     end
   end
 
