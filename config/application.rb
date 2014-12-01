933fea2fc7e0cd33124d72920da8ca9f243bb435
sdlong <sdlong.jeng@gmail.com>
2014-12-01 15:36:46 +0800

    rubocop refactor done

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -13,7 +13,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# rubocop:disable all
 module TechbangRails101
+#rubocop:enable all
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
